2eb8cd644d09d399a4cb84082c8610ce3e2ce0c4
Wolfgang Wohanka <w.wohanka@gmail.com>
2019-07-21 22:10:15 +0200

    add fallback MaterialIcon import

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -223,7 +223,6 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    font-size: 23px;
   }
   .accessibility-menu-item {
     border-radius: 100px;
@@ -273,3 +272,31 @@ export default {
     text-decoration: underline !important;
   }
 </style>
+
+<style scoped>
+  /* fallback */
+  @font-face {
+    font-family: "Material Icons";
+    font-style: normal;
+    font-weight: 400;
+    src: local("Material Icons"), local("MaterialIcons-Regular"),
+      url(https://fonts.gstatic.com/s/materialicons/v17/2fcrYFNaTjcS6g4U3t-Y5ZjZjT5FdEJ140U2DJYC3mY.woff2)
+        format("woff2");
+  }
+  .material-icons {
+    font-family: "Material Icons";
+    font-weight: normal;
+    font-style: normal;
+    font-size: 24px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    -webkit-font-feature-settings: "liga";
+    -webkit-font-smoothing: antialiased;
+  }
+</style>
+
